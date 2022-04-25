5af0240923593a8a5c9ec43e58129605353d97da
estebesov01 <estebesov7777@gmail.com>
2022-04-25 17:55:31 +0600

    last commit

--- neoshop/settings.py
+++ neoshop/settings.py
@@ -86,16 +86,7 @@ WSGI_APPLICATION = 'neoshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT'),
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
